2d0809f618ac16ccd396143996716894a2bd2123
Toki Olaoluwa <tokiolaoluwa01.dev@gmail.com>
2026-09-19 14:33:35 +0100

    refactor: move Flutter app to root and delete backend
    The backend directory has been removed as it's no longer needed.
    Flutter dependencies have been updated to the latest compatible
    versions, including riverpod, talker, and toastification.

--- backend/src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createAccount, login } from "../controllers/user.controller";
-
-const userRouter = express.Router();
-
-userRouter.post("/", createAccount);
-userRouter.post("/login", login);
-
-export default userRouter;
